d20c50aef69cabf09b89ac1832c6c3ec42160e0a
Francisco Turdera <franciscoturderaofficial@gmail.com>
2022-05-05 17:14:42 -0400

    finished random strategy

--- battleship.py
+++ battleship.py
@@ -1,11 +1,31 @@
+from strategies.random_strategy import RandomStrategy
 from structures.direction import Direction
 from structures.field import Field
 
 
+# def play(strategy, dimensions):
+
+
 if __name__ == "__main__":
-    battleship = Field(8, 8)
-    print(battleship.add_boat(5, 0, 0, Direction.SOUTH))
-    print(battleship.add_boat(5, 1, 0, Direction.EAST))
-    print(battleship.hit(0, 0))
-    print(battleship.hit(1, 1))
-    battleship.draw_field()
+    random_strat = RandomStrategy((9, 9), 5, 3, 6, 1)
+    board = random_strat.get_board()
+    # board.draw_field()
+    moves = 0
+    while random_strat.get_remaining_boats() > 0:
+        moves += 1
+        attack_x, attack_y = random_strat.attack()
+        hit = board.hit(attack_x, attack_y)
+        random_strat.feedback(attack_x, attack_y, hit)
+        if hit:
+            board.draw_field()
+            print()
+        # random_strat.play_strategy.opponent.draw_field()
+    print()
+    print("Moves: %s" % moves)
+
+
+# if __name__ == "__main__":
+#     board = Field(4, 4)
+#     board.add_boat(4, 0, 1, Direction.EAST)
+#     board.add_boat(4, 1, 0, Direction.SOUTH)
+#     board.draw_field()
--- strategies/strategy.py
+++ strategies/strategy.py
@@ -4,11 +4,31 @@ from typing import Tuple
 from structures.field import Field
 
 
+class CreateStrategy(ABC):
+    """Base Strategy for creating a board"""
+
+    def __init__(self, dimensions, *boats):
+        self.dimensions = dimensions
+        self.board = self.create_board(*boats)
+        self.populated_spots = sum(boats)
+
+    @abstractmethod
+    def create_board(self, *boats: int) -> Field:
+        """Given a series of lengths of boats, create its playing board"""
+        pass
+
+
 class PlayStrategy(ABC):
     """Abstract Strategy for making attacks"""
 
+    opponent: Field
+    attacked: set
+
     def __init__(self, dimensions):
+        self.dimensions = dimensions
         self.opponent = Field(dimensions[0], dimensions[1])
+        self.attacked = set()
+        self.success = 0
 
     @abstractmethod
     def attack(self) -> Tuple[int, int]:
@@ -21,25 +41,23 @@ class PlayStrategy(ABC):
         pass
 
 
-class CreateStrategy(ABC):
-    """Base Strategy for creating a board"""
-
-    def __init__(self, dimensions):
-        self.dimensions = dimensions
-
-    @abstractmethod
-    def create_board(self, *boats: int) -> Field:
-        """Given a series of lengths of boats, create its playing board"""
-        pass
-
-
 class Strategy:
-    def __init__(self, create_strat: CreateStrategy, play_strat: PlayStrategy, dimensions):
-        self.create_strategy = create_strat(dimensions)
+
+    create_strategy: CreateStrategy
+    play_strategy: PlayStrategy
+
+    def __init__(self, create_strat: CreateStrategy, play_strat: PlayStrategy, dimensions, *boats):
+        self.create_strategy = create_strat(dimensions, *boats)
         self.play_strategy = play_strat(dimensions)
 
     def create_board(self, *boats: int) -> Field:
         return self.create_strategy.create_board(*boats)
+
+    def get_board(self):
+        return self.create_strategy.board
+
+    def get_remaining_boats(self):
+        return self.create_strategy.populated_spots - self.play_strategy.success
 
     def attack(self) -> Tuple[int, int]:
         return self.play_strategy.attack()
