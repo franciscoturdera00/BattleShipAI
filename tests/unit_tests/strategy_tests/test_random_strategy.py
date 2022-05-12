95af9889886291c6fbdfa70c2f2bc644931cd1d6
Francisco Turdera <franciscoturderaofficial@gmail.com>
2022-05-12 19:21:10 -0400

    added more tests, fixed some bugs

--- tests/unit_tests/strategy_tests/test_random_strategy.py
+++ tests/unit_tests/strategy_tests/test_random_strategy.py
@@ -2,8 +2,8 @@ from strategies.random_strategy import RandomCreateStrategy
 
 
 def test_random_create():
-    create = RandomCreateStrategy((8, 8), None, 5, 4, 3, 2, 1)
-    board = create.board
+    create = RandomCreateStrategy((8, 8))
+    board = create.create_board(5, 4, 3, 2, 1)
     assert board is not None
     count = 0
     for column in board.spaces:
